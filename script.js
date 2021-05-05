044617501bec70af987d0aa9f6ab1c3ef8672156
jose-gonzalez-46 <jose.gonzalezo@duocuc.cl>
2021-05-05 00:46:22 -0400

    se agrego el script con la primera parte de la validacion del formulario

---
+++ script.js
@@ -0,0 +1,35 @@
+var expr = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+
+$(document).ready(function() {
+    $("#uname").mouseleave(function() {
+        var nombre = $("#uname").val();
+
+
+        if (nombre == "") {
+            $("#mensaje1").fadeIn();
+            return false;
+        } else {
+            $("#mensaje1").fadeOut();
+
+        }
+
+    });
+});
+
+
+$(document).ready(function() {
+    $("#email").mouseleave(function() {
+        var correo = $("#email").val();
+
+
+        if (correo == "" || !expr.test(correo)) {
+            $("#mensaje2").fadeIn();
+            return false;
+
+        } else {
+            $("#mensaje2").fadeOut();
+
+        }
+
+    });
+});
